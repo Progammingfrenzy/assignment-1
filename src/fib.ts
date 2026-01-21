9975cc0732704b276b7fa65617108e4ba4e677ba
Progammingfrenzy <69093164+Progammingfrenzy@users.noreply.github.com>
2026-01-20 19:37:55 -0500

    Fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,14 @@
-// util function that computes the fibonacci numbers
-export default function fibonacci(n) {
+export default function fibonacci(n: number): number {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  const a = fibonacci(n - 1) as number;
+  const b = fibonacci(n - 2) as number;
+  return a + b;
 }
+
